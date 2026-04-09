f6a0708e9c3984d5450fde4fddf3f377d4d638c6
Simpy Kumari Mandal <simkmandal01@gmail.com>
2026-04-10 01:29:29 +0530

    Add files via upload

---
+++ ingredient2recipe/config.js
@@ -0,0 +1,30 @@
+window.APP_CONFIG = {
+  SPOONACULAR_API_KEY: "72d4616576b24b4c95d4c093d62bcc14",
+  OPENVISION_API_URL: "https://api.openvisionapi.com/analyze",
+  BLOG_RECIPES: [
+    {
+      title: "Chana Masala Recipe (Chickpea Masala)",
+      url: "https://www.indianhealthyrecipes.com/chana-masala/",
+    },
+    {
+      title: "Aloo Gobi Recipe (Cauliflower Potato Curry)",
+      url: "https://www.indianhealthyrecipes.com/aloo-gobi-recipe/",
+    },
+    {
+      title: "Chicken Tikka Masala Recipe",
+      url: "https://www.indianhealthyrecipes.com/chicken-tikka-masala/",
+    },
+    {
+      title: "Tandoori Chicken Recipe",
+      url: "https://www.indianhealthyrecipes.com/tandoori-chicken-recipe/",
+    },
+    {
+      title: "Dal Makhani Recipe",
+      url: "https://www.indianhealthyrecipes.com/dal-makhani-recipe/",
+    },
+    {
+      title: "Butter Chicken Recipe",
+      url: "https://www.indianhealthyrecipes.com/butter-chicken/",
+    },
+  ],
+};
